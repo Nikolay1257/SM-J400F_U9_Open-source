3654746beb1d84f89e85e42c734352e2f86b6e01
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-10-01 22:20:21 +0700

    remove sdfat_debug_warn_on call

--- fs/sdfat/blkdev.c
+++ fs/sdfat/blkdev.c
@@ -159,7 +159,7 @@ s32 bdev_mread(struct super_block *sb, u64 secno, struct buffer_head **bh, u64 n
 	if (!(fsi->prev_eio & SDFAT_EIO_READ)) {
 		fsi->prev_eio |= SDFAT_EIO_READ;
 		sdfat_log_msg(sb, KERN_ERR, "%s: No bh. I/O error.", __func__);
-		sdfat_debug_warn_on(1);
+		//sdfat_debug_warn_on(1);
 	}
 
 	return -EIO;
