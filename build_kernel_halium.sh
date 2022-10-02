3eb96d2fdc920b64652ce9855a0da114b1476207
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-10-02 18:43:05 +0700

    don't set ANDROID_MAJOR_VERSION

--- build_kernel_halium.sh
+++ build_kernel_halium.sh
@@ -3,7 +3,6 @@
 export ARCH=arm64
 #YOU CAN DOWNLOAD CROSS COMPILER FROM https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 
 export CROSS_COMPILE=$(pwd)/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export ANDROID_MAJOR_VERSION=q
 
 make exynos7570-j4lte-halium_defconfig
 make -j2
