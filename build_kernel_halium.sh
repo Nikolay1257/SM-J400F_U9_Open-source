87bc2d6f3a2b982e39fc981d390fa4bae0f08635
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-10-02 13:58:53 +0700

    added halium support(currently it crash on early init)

--- build_kernel_halium.sh
+++ build_kernel_halium.sh
@@ -5,7 +5,7 @@ export ARCH=arm64
 export CROSS_COMPILE=$(pwd)/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=q
 
-make exynos7570-j4lte_defconfig
+make exynos7570-j4lte-halium_defconfig
 make -j2
-cp arch/arm64/boot/Image kernel.bin
+cp arch/arm64/boot/Image kernel-halium.bin
 make mrproper
