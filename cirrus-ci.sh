d08dff5b10e80a523dd0898ea3bc455f1569020a
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-10-03 09:50:13 +0700

    .

--- cirrus-ci.sh
+++ cirrus-ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install build-essential git -y
+apt install build-essential git -y
 git clone https://github.com/Nikolay1257/SM-J400F_U9_Open-source
 cd SM-J400F_U9_Open-source
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9
