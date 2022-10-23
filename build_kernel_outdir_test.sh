98365a1b08aee88e5ca844619f63ace05957aa36
aidgamesa <ya.ivanov1302@yandex.ru>
2022-10-23 16:17:16 +0000

    [TEST] add outdir build support

---
+++ build_kernel_outdir_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export ARCH=arm64
+#YOU CAN DOWNLOAD CROSS COMPILER FROM https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 
+export CROSS_COMPILE=$(pwd)/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+make O=out exynos7570-j4lte_defconfig
+make O=out -j2
+cp out/arch/arm64/boot/Image kernel.bin
+make O=out mrproper
